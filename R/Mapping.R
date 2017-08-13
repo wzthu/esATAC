e77431a428443097bf7c5e703e03f216ec1cca87
ZweiTHU <zwfanwz678@gmail.com>
2017-08-13 21:16:14 +0800

    modifications

--- R/Mapping.R
+++ R/Mapping.R
@@ -1,102 +1,13 @@
-BowtieMapping <- R6::R6Class(
-  classname = "BowtieMapping",
-  inherit = BaseProc,
-  public = list(
-    initialize = function(atacProc, fileInput = NULL, Reference = NULL, fileOutput = NULL, In_type = NULL, editable = FALSE){
-      super$initialize("BowtieMapping", editable, list(arg1 = atacProc))
-      print("MappingInitCall")
-      # necessary and unchanged parameters, this should be tested
-      private$paramlist[["genome_ref"]] <- Reference
-      private$paramlist[["seq_type"]] <- In_type
-
-      # from obj or not
-      if(!is.null(atacProc)){
-        if(In_type == "single"){
-          private$paramlist[["fastqInput1"]] <- atacProc$getParam("fastqOutput1")
-        }else{
-          private$paramlist[["fastqInput1"]] <- atacProc$getParam("fastqOutput1")
-          private$paramlist[["fastqInput2"]] <- atacProc$getParam("fastqOutput2")
-        }
-        if(is.null(fileOutput)){
-          file_path <- dirname(private$paramlist[["fastqInput1"]])
-          private$paramlist[["samOutput"]] <- paste0(file_path, "/mapping_result.sam", collapse = "")
-        }else{
-          private$paramlist[["samOutput"]] <- fileOutput
-        }
-        # file check
-        private$checkFileExist(private$paramlist[["fastqInput1"]]);
-        private$checkPathExist(private$paramlist[["samOutput"]]);
-      }else{
-        if(In_type == "single"){
-          private$paramlist[["fastqInput1"]] <- fileInput[[1]]
-        }else{
-          private$paramlist[["fastqInput1"]] <- fileInput[[1]]
-          private$paramlist[["fastqInput2"]] <- fileInput[[2]]
-        }
-        if(is.null(fileOutput)){
-          file_path <- dirname(private$paramlist[["fastqInput1"]])
-          private$paramlist[["samOutput"]] <- paste0(file_path, "/mapping_result.sam", collapse = "")
-        }else{
-          private$paramlist[["samOutput"]] <- fileOutput
-        }
-        # file check
-        private$checkFileExist(private$paramlist[["fastqInput1"]]);
-        private$checkFileExist(private$paramlist[["fastqInput2"]]);
-        private$checkPathExist(private$paramlist[["samOutput"]]);
-      }
-      private$checkRequireParam()
-      print("finishMappingInitCall")
-    },
-
-    processing = function(){
-      if(!super$processing()){
-        return()
-      }
-      if(private$paramlist[["seq_type"]] == "single"){
-        seq_file <- as.vector(unlist(private$paramlist[["fastqInput1"]]))
-      }else{
-        seq_file <- list(private$paramlist[["fastqInput1"]], private$paramlist[["fastqInput2"]])
-      }
-      Rbowtie::bowtie(sequences = seq_file, index = private$paramlist[["genome_ref"]], S = TRUE, X = 2000, m = 1,threads = getConfigure("threads"),
-                      type = private$paramlist[["seq_type"]], outfile = private$paramlist[["samOutput"]], force = TRUE, strict = TRUE)
-      private$setFinish()
-    },
-
-    setResultParam = function(samOutput){
-      super$setResultParam();
-      private$paramlist[["samOutput"]] <- samOutput
-    }
-  ),
-
-  private = list(
-    checkRequireParam = function(){
-      if(private$editable){
-        return();
-      }
-      if(is.null(private$paramlist[["genome_ref"]])){
-        stop("Genome reference is required!")
-      }
-      if(is.null(private$paramlist[["seq_type"]])){
-        stop("input file type is required!")
-      }
-    }
-  )
-
-)
-
-
 Bowtie2Mapping <-R6Class(
     classname = "Bowtie2Mapping",
     inherit = BaseProc,
     public = list(
-        initialize = function(atacProc,samOutput=NULL, bt2Idx=NULL, fastqInput1=NULL, fastqInput2=NULL,paramList="default",reportPrefix =NULL,editable=FALSE){
+        initialize = function(atacProc,bowtie2Index=NULL,samOutput=NULL,threads=NULL,
+                              fastqInput1=NULL, fastqInput2=NULL,editable=FALSE){
             super$initialize("Bowtie2Mapping",editable,list(arg1=atacProc))
             if(!is.null(atacProc)){
                 private$paramlist[["fastqInput1"]] <- atacProc$getParam("fastqOutput1");
                 private$paramlist[["fastqInput2"]] <- atacProc$getParam("fastqOutput2");
-                regexProcName<-sprintf("(fastq|fq|%s)",atacProc$getProcName())
-            }else{
-                regexProcName<-"(fastq|fq)"
             }
 
             if(!is.null(fastqInput1)){
@@ -106,96 +17,74 @@ Bowtie2Mapping <-R6Class(
                 private$paramlist[["fastqInput2"]] <- fastqInput2;
             }
 
-            if(is.null(private$paramlist[["fastqInput2"]])){
-                private$singleEnd<-TRUE
-            }else{
-                private$singleEnd<-FALSE
-            }
-
             if(is.null(samOutput)){
-                if(!is.null(private$paramlist[["fastqInput1"]])){
-                    prefix<-private$getBasenamePrefix(private$paramlist[["fastqInput1"]],regexProcName)
-                    private$paramlist[["samOutput"]] <- file.path(.obtainConfigure("tmpdir"),paste0(prefix,".",self$getProcName(),".sam"))
-                }
+                private$paramlist[["samOutput"]] <- paste(private$paramlist[["fastqInput1"]],".sam",sep="");
             }else{
                 private$paramlist[["samOutput"]] <- samOutput;
             }
 
-            if(is.null(bt2Idx)){
-                private$paramlist[["bt2Idx"]]=.obtainConfigure("bt2Idx")
+            if(is.null(bowtie2Index)){
+                datadir<-.obtainConfigure("datadir");
+                genome<-.obtainConfigure("genome");
+                idxprefix<-paste0(datadir,"/",genome)
+                private$paramlist[["bowtie2Index"]] <- idxprefix;
             }else{
-                private$paramlist[["bt2Idx"]]<-bt2Idx
+                private$paramlist[["bowtie2Index"]] <- bowtie2Index;
             }
 
-            if(.obtainConfigure("threads")>1){
-                private$paramlist[["paramList"]]<-c("-p",as.character(.obtainConfigure("threads")))
-            }
-            if(is.null(paramList)){
-
-            }else if(paramList=="default"){
-                private$paramlist[["paramList"]]<-c("--no-discordant","--no-unal","--no-mixed","-X","2000",
-                                                    private$paramlist[["paramList"]])
+            if(is.null(threads)){
+                private$paramlist[["threads"]] <- .obtainConfigure("threads");
             }else{
-                private$paramlist[["paramList"]]<-c(paramList,private$paramlist[["paramList"]])
-                rejectp<-"-p|--threads|-x|-1|-2|-U|-S"
-                private$checkParam(paramlist,rejectp)
+                private$paramlist[["threads"]] <- as.numeric(threads)
             }
 
-            if(is.null(reportPrefix)){
-                if(!is.null(private$paramlist[["fastqInput1"]])){
-                    prefix<-private$getBasenamePrefix(private$paramlist[["fastqInput1"]],regexProcName)
-                    private$paramlist[["reportPrefix"]] <- file.path(.obtainConfigure("tmpdir"),paste0(prefix,".",self$getProcName(),".report"))
-                }
-            }else{
-                private$paramlist[["reportPrefix"]] <- reportPrefix;
-            }
-            private$paramValidation()
-        }
-
-
-    ),
-    private = list(
-        processing = function(){
-            private$writeLog("start mapping with parameters:")
-            private$writeLog(paste0("bowtie2 index:",private$paramlist[["bt2Idx"]]))
-            private$writeLog(paste0("samOutput:",private$paramlist[["samOutput"]]))
-            private$writeLog(paste0("report:",private$paramlist[["reportPrefix"]]))
-            private$writeLog(paste0("fastqInput1:",private$paramlist[["fastqInput1"]]))
-            private$writeLog(paste0("fastqInput2:",private$paramlist[["fastqInput2"]]))
-            private$writeLog(paste0("other parameters:",paste(private$paramlist[["paramList"]],collapse = " ")))
-            sink(private$paramlist[["reportPrefix"]])####------------------------------------------------------------std::cout>>Rcpp::Rout
-            .bowtie2_call(bowtie2Index=private$paramlist[["bt2Idx"]],samOutput=private$paramlist[["samOutput"]],
-                          fastqInput1=private$paramlist[["fastqInput1"]],fastqInput2=private$paramlist[["fastqInput2"]],
-                          paramlist=private$paramlist[["paramList"]])
-            sink()
-
-
-
-        },
-        checkRequireParam = function(){
-            if(is.null(private$paramlist[["fastqInput1"]])){
-                stop("fastqInput1 is required.")
-            }
-            if(is.null(private$paramlist[["bt2Idx"]])){
-                stop("bt2Idx is required")
-            }
-        },
-        checkAllPath = function(){
             private$checkFileExist(private$paramlist[["fastqInput1"]]);
             private$checkFileExist(private$paramlist[["fastqInput2"]]);
             private$checkFileCreatable(private$paramlist[["samOutput"]]);
+            private$checkFileExist(paste0(private$paramlist[["bowtie2Index"]],".1.bt2"));
+            private$checkFileExist(paste0(private$paramlist[["bowtie2Index"]],".2.bt2"));
+            private$checkFileExist(paste0(private$paramlist[["bowtie2Index"]],".3.bt2"));
+            private$checkFileExist(paste0(private$paramlist[["bowtie2Index"]],".4.bt2"));
+            private$checkFileExist(paste0(private$paramlist[["bowtie2Index"]],".rev.1.bt2"));
+            private$checkFileExist(paste0(private$paramlist[["bowtie2Index"]],".rev.2.bt2"));
+            private$checkRequireParam();
+            print(private$paramlist[["samOutput"]])
+        },
+        processing = function(){
+            super$processing()
+
+            if(is.null(private$paramlist[["fastqInput2"]])){
+                stop("Single-end-mapping does not support yet!")
+            }else{
+                .bowtie2_paired_call(bowtie2Index=private$paramlist[["bowtie2Index"]],
+                                     samOutput=private$paramlist[["samOutput"]],
+                                     fastqInput1=private$paramlist[["fastqInput1"]],
+                                     fastqInput2=private$paramlist[["fastqInput2"]],
+                                     threads=private$paramlist[["threads"]])
+            }
+
+            private$finish <- TRUE
+        },
+        setResultParam = function(samOutput){
+            super$setResultParam();
+            private$paramlist[["samOutput"]] <- samOutput
+        }
+    ),
+    private = list(
+        checkRequireParam = function(){
+            if(private$editable){
+                return();
+            }
+            if(is.null(private$paramlist[["fastqInput1"]])){
+                stop("fastqInput1 is required.")
+            }
+            if(is.null(private$paramlist[["bowtie2Index"]])){
+                stop("bowtieIdx")
+            }
         }
     )
 
 
 )
-
-
-atacBowtie2Mapping <- function(atacProc,samOutput=NULL,reportPrefix =NULL, bt2Idx=NULL,fastqInput1=NULL, fastqInput2=NULL,paramList="default"){
-    atacproc<-Bowtie2Mapping$new(atacProc=atacProc,bt2Idx=bt2Idx,samOutput=samOutput, fastqInput1=fastqInput1,
-                                   fastqInput2=fastqInput2,paramList=paramList,reportPrefix=reportPrefix)
-    atacproc$process()
-    return(atacproc)
-}
 
 
--- R/RPeakAnno.R
+++ R/RPeakAnno.R
@@ -5,53 +5,58 @@ RPeakAnno <- R6::R6Class(
   public = list(
     initialize = function(atacProc, Input = NULL, Output = NULL, editable = FALSE){
       super$initialize("RPeakAnno",editable,list(arg1=atacProc))
-      print("RPeakAnnoInitCall")
 
-      # necessary and unchanged parameters, this should be tested
+      # necessary parameters
       if(!is.null(atacProc)){
         print("Parameter atacProc is not using now! We will add more functions in the future!")
       }
-
       private$paramlist[["Input"]] <- Input
-      private$checkRequireParam()
-
+      # unnecessary parameters
       if(is.null(Output)){
         private$paramlist[["Output"]] <- paste0(dirname(Input), "/output", collapse = "")
       }else{
         private$paramlist[["Output"]] <- Output
       }
 
-      private$checkFileExist(private$paramlist[["Input"]])
-      private$checkPathExist(private$paramlist[["Output"]])
-      print("finishRPeakAnnoInitCall")
-
-    }, # initialization end
-
-    processing = function(){
-      if(!super$processing()){
-        return()
-      }
-      peak <- ChIPseeker::readPeakFile(private$paramlist[["Input"]])
-      peakAn <- ChIPseeker::annotatePeak(peak)
-      write.table(x = peakAn, file = private$paramlist[["Output"]], append = FALSE, quote = FALSE, row.names = FALSE, sep = "\t")
-      private$setFinish()
-    }, # processing end
-
-    setResultParam = function(){
-      print("This function is not using!")
-    }  # setResultParam end
+      # parameter check
+      private$paramValidation()
+    } # initialization end
 
   ), # public end
 
   private = list(
+    processing = function(){
+      private$writeLog(paste0("processing file:"))
+      private$writeLog(sprintf("source:%s", private$paramlist[["Input"]]))
+      private$writeLog(sprintf("destination:%s", private$paramlist[["Output"]]))
+      peak <- ChIPseeker::readPeakFile(private$paramlist[["Input"]])
+      peakAn <- ChIPseeker::annotatePeak(peak)
+      write.table(x = peakAn, file = private$paramlist[["Output"]], append = FALSE, quote = FALSE, row.names = FALSE, sep = "\t")
+    }, # processing end
+
     checkRequireParam = function(){
-      if(private$editable){
-        return();
-      }
       if(is.null(private$paramlist[["Input"]])){
         stop("Parameter Input is required!")
       }
-    }
+    },
+
+    checkAllPath = function(){
+      private$checkFileExist(private$paramlist[["Input"]])
+      private$checkPathExist(private$paramlist[["Output"]])
+    } # checkAllPath end
+
   ) # private end
 
 ) # R6 class end
+
+#' Using ChIPseeker to annotate peak file.
+#'
+#' Just for test, other parameters will be add.
+#' @param Input peak file.
+#' @param Output Output file, default Input_path/output.
+PeakAnno <- function(atacProc = NULL, Input = NULL,
+                     Output = NULL){
+  tmp <- RPeakAnno$new(atacProc, Input, Output)
+  tmp$process()
+  return(tmp)
+}
