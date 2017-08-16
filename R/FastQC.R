edb8a42d51d880e6515633c9e4384891a2e3c496
wzthu <wzweizheng@qq.com>
2017-08-16 10:02:31 +0800

    merge

--- R/FastQC.R
+++ R/FastQC.R
@@ -4,46 +4,33 @@ FastQC <- R6::R6Class(
   public = list(
     initialize = function(atacProc, input_file = NULL, output_file = NULL, editable = FALSE){
       super$initialize("FastQC", editable, list(arg1 = atacProc))
-      print("QCreporterInitCall")
+
       # necessary and unchanged parameters, this should be tested
       # in this class, there is no necessary and unchanged parameters
       # input processing
-      private$checkRequireParam()
       if((!is.null(atacProc)) && (class(atacProc)[1] == "UnzipAndMerge")){ # atacproc from UnzipAndMerge
         print("UnzipAndMerge")
         if(is.null(atacProc$getParam("fastqOutput2"))){ # single end
           private$paramlist[["Input1"]] <- as.vector(unlist(atacProc$getParam("fastqOutput1")))
-          # input file check
-          private$checkFileExist(private$paramlist[["Input1"]]);
           private$paramlist[["Input"]] <- c(private$paramlist[["Input1"]])
         }else{ # paired end
           private$paramlist[["Input1"]] <- as.vector(unlist(atacProc$getParam("fastqOutput1")))
           private$paramlist[["Input2"]] <- as.vector(unlist(atacProc$getParam("fastqOutput2")))
-          # input file check
-          private$checkFileExist(private$paramlist[["Input1"]]);
-          private$checkFileExist(private$paramlist[["Input2"]]);
           private$paramlist[["Input"]] <- c(private$paramlist[["Input1"]], private$paramlist[["Input2"]])
         }
       }else if((!is.null(atacProc)) && (class(atacProc)[1] == "Renamer")){ # atacproc from renamer
         print("Renamer")
         if(is.null(atacProc$getParam("fastqOutput2"))){ # single end
           private$paramlist[["Input1"]] <- as.vector(unlist(atacProc$getParam("fastqOutput1")))
-          # input file check
-          private$checkFileExist(private$paramlist[["Input1"]]);
           private$paramlist[["Input"]] <- c(private$paramlist[["Input1"]])
         }else{ # paired end
           private$paramlist[["Input1"]] <- as.vector(unlist(atacProc$getParam("fastqOutput1")))
           private$paramlist[["Input2"]] <- as.vector(unlist(atacProc$getParam("fastqOutput2")))
-          # input file check
-          private$checkFileExist(private$paramlist[["Input1"]]);
-          private$checkFileExist(private$paramlist[["Input2"]]);
           private$paramlist[["Input"]] <- c(private$paramlist[["Input1"]], private$paramlist[["Input2"]])
         }
       }else if((!is.null(atacProc)) && (class(atacProc)[1] == "SamToBam")){
         print("SamToBam")
         private$paramlist[["Input1"]] <- as.vector(unlist(atacProc$getParam("bamOutput")))
-        # input file check
-        private$checkFileExist(private$paramlist[["Input1"]]);
         private$paramlist[["Input"]] <- c(private$paramlist[["Input1"]])
       }else if(((!is.null(atacProc)) && (class(atacProc)[1] != "UnzipAndMerge")) ||
                ((!is.null(atacProc)) && (class(atacProc)[1] != "Renamer")) ||
@@ -51,15 +38,10 @@ FastQC <- R6::R6Class(
         stop("Input class must be got from 'UnzipAndMerge' or 'Renamer' or 'SamToBam'!")
       }else if(is.null(atacProc) && (length(input_file) == 1)){ # input one file
         private$paramlist[["Input1"]] <- as.vector(unlist(input_file))
-        # input file check
-        private$checkFileExist(private$paramlist[["Input1"]]);
         private$paramlist[["Input"]] <- c(private$paramlist[["Input1"]])
       }else if(is.null(atacProc) && (length(input_file) == 2)){ # input two file
         private$paramlist[["Input1"]] <- as.vector(unlist(input_file))[1]
         private$paramlist[["Input2"]] <- as.vector(unlist(input_file))[2]
-        # input file check
-        private$checkFileExist(private$paramlist[["Input1"]]);
-        private$checkFileExist(private$paramlist[["Input2"]]);
         private$paramlist[["Input"]] <- c(private$paramlist[["Input1"]], private$paramlist[["Input2"]])
       }
       #output processing
@@ -69,31 +51,44 @@ FastQC <- R6::R6Class(
       }else{
         private$paramlist[["Output"]] <- output_file
       }
-      # output path check
-      private$checkPathExist(private$paramlist[["Output"]]);
 
-      print("finishQCreporterInitCall")
-    },
-
-    processing = function(){
-      super$processing()
-      print(private$paramlist[["Input"]])
-      QuasR::qQCReport(input = private$paramlist[["Input"]], pdfFilename = private$paramlist[["Output"]])
-      private$finish <- TRUE
-    },
-
-    setResultParam = function(FilePath){
-      super$setResultParam();
-      private$paramlist[["Output"]] <- FilePath
-    }
+      # parameter check
+      private$paramValidation()
+    } # initialization end
 
   ),
 
   private = list(
+    processing = function(){
+      private$writeLog(paste0("processing file:"))
+      private$writeLog(sprintf("source:%s", private$paramlist[["Input"]]))
+      private$writeLog(sprintf("destination:%s", private$paramlist[["Output"]]))
+      print(private$paramlist[["Input"]])
+      QuasR::qQCReport(input = private$paramlist[["Input"]], pdfFilename = private$paramlist[["Output"]])
+    }, # processing end
+
     checkRequireParam = function(){
-      if(private$editable){
-        return();
+      if(is.null(private$paramlist[["Input1"]])){
+        stop("Parameter input_file is required!")
       }
-    }
-  )
-)
+    }, # checkRequireParam end
+
+    checkAllPath = function(){
+      private$checkFileExist(private$paramlist[["Input1"]])
+      private$checkPathExist(private$paramlist[["Output"]])
+    } # checkAllPath end
+
+  ) # private end
+
+) # class end
+
+
+
+#' Quality control using Quasr::qQCreport
+#' @param input_file a vector.
+#' @export
+atacQCReport <- function(atacProc = NULL, input_file = NULL, output_file = NULL){
+  tmp <- FastQC$new(atacProc, input_file, output_file)
+  tmp$process()
+  return(tmp)
+}
