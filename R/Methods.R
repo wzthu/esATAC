a66a3d51b484f6cbb6c79ed3ab31d57656704994
wzthu <wzweizheng@qq.com>
2017-08-16 10:05:01 +0800

    recover Methosds

---
+++ R/Methods.R
@@ -0,0 +1,41 @@
+atacPipe <- function(fastqInput1,fastqInput2=NULL,tmpdir=NULL,saveTmpFiles=FALSE,renamer=TRUE,removeAdapter=TRUE){
+    atacInputFile(fastqInput1 = fastqInput1, fastqInput2 = fastqInput2)
+    gph<-GraphMng$new()
+    vlist<-gph$getProcList();
+    if(!renamer){
+        vlist$renamer<-NULL
+    }
+    if(!removeAdapter){
+        vlist$removeAdapter<-NULL
+    }
+    return(gph$getSubGraphTopo(as.numeric(vlist),atacInputFile,list(Renamer=Renamer,RemoveAdapter=RemoveAdapter)))
+}
+
+atacPrintMap <-function(atacProc=NULL,preProc=FALSE,nextProc=TRUE,curProc=TRUE,display=TRUE){
+    if(is.null(atacProc)){
+        GraphMng$new()$printMap(display=display)
+    }else if(class(atacProc)=="character"){
+        GraphMng$new()$printMap(atacProc,preProc,nextProc,curProc,display=display)
+    }else{
+        atacProc$printMap(preProc,nextProc,curProc,display=display)
+        return(atacProc)
+    }
+}
+
+atacPrintNextList<-function(atacProc){
+    if(class(atacProc)=="character"){
+        GraphMng$new()$getNextProcs(atacProc)
+    }else{
+        GraphMng$new()$getNextProcs(atacProc$getProcName())
+        return(atacProc)
+    }
+}
+
+atacPrintPrevList<-function(atacProc){
+    if(class(atacProc)=="character"){
+        GraphMng$new()$getPrevProcs(atacProc)
+    }else{
+        GraphMng$new()$getPrevProcs(atacProc$getProcName())
+        return(atacProc)
+    }
+}
