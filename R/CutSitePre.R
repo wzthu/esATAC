f759985cc31ef45315ada503401e862bf71a7548
ZweiTHU <zwfanwz678@gmail.com>
2017-08-23 15:53:17 +0800

    modify para and names

--- R/CutSitePre.R
+++ R/CutSitePre.R
@@ -3,18 +3,21 @@ CutSitePre <- R6::R6Class(
   inherit = BaseProc,
 
   public = list(
-    initialize = function(atacProc, InputFile = NULL, OutputFile = NULL, prefix = NULL, editable = FALSE){
+    initialize = function(atacProc, bedInput = NULL, csOutput = NULL, prefix = NULL, editable = FALSE){
       super$initialize("CutSitePre",editable,list(arg1=atacProc))
 
       # necessary parameters
-      private$paramlist[["Input"]] <- InputFile
+      if(!is.null(atacProc)){ # get parameter from class BamToBed or SamToBed
+        private$paramlist[["bedInput"]] <- atacProc$getParam("bedOutput");
+      }else{
+        private$paramlist[["bedInput"]] <- bedInput
+      }
       # unnecessary parameters
       if(is.null(prefix)){
-        private$paramlist[["Output"]] <- paste0(OutputFile, "/", "output_", collapse = "")
+        private$paramlist[["csOutput"]] <- paste0(csOutput, "/", "output", collapse = "")
       }else{
-        private$paramlist[["Output"]] <- paste0(OutputFile, "/", prefix, "_", collapse = "")
+        private$paramlist[["csOutput"]] <- paste0(csOutput, "/", prefix, collapse = "")
       }
-
       # parameter check
       private$paramValidation()
     } # initialization end
@@ -25,23 +28,23 @@ CutSitePre <- R6::R6Class(
   private = list(
     processing = function(){
       private$writeLog(paste0("processing file:"))
-      private$writeLog(sprintf("source:%s", private$paramlist[["Input"]]))
-      private$writeLog(sprintf("destination:%s", private$paramlist[["Output"]]))
-      .CutSite_call(InputFile = private$paramlist[["Input"]], OutputFile = private$paramlist[["Output"]])
+      private$writeLog(sprintf("source:%s", private$paramlist[["bedInput"]]))
+      private$writeLog(sprintf("destination:%s", private$paramlist[["csOutput"]]))
+      .CutSite_call(InputFile = private$paramlist[["bedInput"]], OutputFile = private$paramlist[["csOutput"]])
     }, # processing end
 
     checkRequireParam = function(){
-      if(is.null(private$paramlist[["Input"]])){
-        stop("Parameter InputFile is required!")
+      if(is.null(private$paramlist[["bedInput"]])){
+        stop("Parameter bedInput is required!")
       }
-      if(is.null(private$paramlist[["Output"]])){
-        stop("Parameter OutputFile is required!")
+      if(is.null(private$paramlist[["csOutput"]])){
+        stop("Parameter csOutput is required! An empty file is recommented!")
       }
     }, # checkRequireParam end
 
     checkAllPath = function(){
-      private$checkFileExist(private$paramlist[["Input"]])
-      private$checkPathExist(private$paramlist[["Output"]])
+      private$checkFileExist(private$paramlist[["bedInput"]])
+      private$checkPathExist(private$paramlist[["csOutput"]])
     } # checkAllPath end
 
   ) # private end
@@ -51,12 +54,13 @@ CutSitePre <- R6::R6Class(
 
 #' extract cut site information
 #' @param atacProc Do not use this parameter, we will add nore functions in the future!
-#' @param InputFile Input file path, the No.1-3 column is chromatin name, start cut site, end cut site.
-#' @param OutputFile The output path, an empty folder would be great.
+#' @param bedInput Input file path, the No.1-3 column is chromatin name, start cut site, end cut site.
+#' The merged bed file is recommented.
+#' @param csOutput The output path, an empty folder would be great.
 #' @param prefix Output file name prefix, e.g. prefix_chr*.bed, default "output".
 #' @export
-atacCutSitePre <- function(atacProc = NULL, InputFile = NULL, OutputFile = NULL, prefix = NULL){
-  tmp <- CutSitePre$new(atacProc, InputFile, OutputFile, prefix)
+atacCutSitePre <- function(atacProc = NULL, bedInput = NULL, csOutput = NULL, prefix = NULL){
+  tmp <- CutSitePre$new(atacProc, bedInput, csOutput, prefix)
   tmp$process()
   return(tmp)
 }
--- R/RPeakAnno.R
+++ R/RPeakAnno.R
@@ -3,19 +3,42 @@ RPeakAnno <- R6::R6Class(
   inherit = BaseProc,
 
   public = list(
-    initialize = function(atacProc, Input = NULL, Output = NULL, editable = FALSE){
+    initialize = function(atacProc, peakInput = NULL, tssRegion = NULL, TxDb = NULL,
+                          level = NULL, assignGenomicAnnotation = NULL,
+                          genomicAnnotationPriority = NULL,
+                          annoDb = NULL, addFlankGeneInfo = NULL,
+                          flankDistance = NULL, sameStrand = NULL, ignoreOverlap = NULL,
+                          ignoreUpstream = NULL, ignoreDownstream = NULL, overlap = NULL,
+                          verbose = NULL, annoOutput = NULL, editable = FALSE){
       super$initialize("RPeakAnno",editable,list(arg1=atacProc))
 
       # necessary parameters
       if(!is.null(atacProc)){
-        print("Parameter atacProc is not using now! We will add more functions in the future!")
-      }
-      private$paramlist[["Input"]] <- Input
-      # unnecessary parameters
-      if(is.null(Output)){
-        private$paramlist[["Output"]] <- paste0(dirname(Input), "/output", collapse = "")
+        private$paramlist[["peakInput"]] <- atacProc$getParam("bedOutput");
       }else{
-        private$paramlist[["Output"]] <- Output
+        private$paramlist[["peakInput"]] <- peakInput
+      }
+      private$paramlist[["tssRegion"]] <- tssRegion
+      private$paramlist[["TxDb"]] <- TxDb
+      private$paramlist[["level"]] <- level
+      private$paramlist[["assignGenomicAnnotation"]] <- assignGenomicAnnotation
+      private$paramlist[["genomicAnnotationPriority"]] <- genomicAnnotationPriority
+      private$paramlist[["annoDb"]] <- annoDb
+      private$paramlist[["addFlankGeneInfo"]] <- addFlankGeneInfo
+      private$paramlist[["flankDistance"]] <- flankDistance
+      private$paramlist[["sameStrand"]] <- sameStrand
+      private$paramlist[["ignoreOverlap"]] <- ignoreOverlap
+      private$paramlist[["ignoreUpstream"]] <- ignoreUpstream
+      private$paramlist[["ignoreDownstream"]] <- ignoreDownstream
+      private$paramlist[["overlap"]] <- overlap
+      private$paramlist[["verbose"]] <- verbose
+
+      # unnecessary parameters
+      if(is.null(annoOutput)){
+        private$paramlist[["annoOutput"]] <- paste0(dirname(private$paramlist[["peakInput"]]),
+                                                    "/output", collapse = "")
+      }else{
+        private$paramlist[["annoOutput"]] <- annoOutput
       }
 
       # parameter check
@@ -27,22 +50,37 @@ RPeakAnno <- R6::R6Class(
   private = list(
     processing = function(){
       private$writeLog(paste0("processing file:"))
-      private$writeLog(sprintf("source:%s", private$paramlist[["Input"]]))
-      private$writeLog(sprintf("destination:%s", private$paramlist[["Output"]]))
-      peak <- ChIPseeker::readPeakFile(private$paramlist[["Input"]])
-      peakAn <- ChIPseeker::annotatePeak(peak)
-      write.table(x = peakAn, file = private$paramlist[["Output"]], append = FALSE, quote = FALSE, row.names = FALSE, sep = "\t")
+      private$writeLog(sprintf("source:%s", private$paramlist[["peakInput"]]))
+      private$writeLog(sprintf("destination:%s", private$paramlist[["annoOutput"]]))
+      peak <- ChIPseeker::readPeakFile(private$paramlist[["peakInput"]])
+      peakAn <- ChIPseeker::annotatePeak(peak = peak,
+                                         tssRegion = private$paramlist[["tssRegion"]],
+                                         TxDb = private$paramlist[["TxDb"]],
+                                         level = private$paramlist[["level"]],
+                                         assignGenomicAnnotation = private$paramlist[["assignGenomicAnnotation"]],
+                                         genomicAnnotationPriority = private$paramlist[["genomicAnnotationPriority"]],
+                                         annoDb = private$paramlist[["annoDb"]],
+                                         addFlankGeneInfo = private$paramlist[["addFlankGeneInfo"]],
+                                         flankDistance = private$paramlist[["flankDistance"]],
+                                         sameStrand = private$paramlist[["sameStrand"]],
+                                         ignoreOverlap = private$paramlist[["ignoreOverlap"]],
+                                         ignoreUpstream = private$paramlist[["ignoreUpstream"]],
+                                         ignoreDownstream = private$paramlist[["ignoreDownstream"]],
+                                         overlap = private$paramlist[["overlap"]],
+                                         verbose = private$paramlist[["verbose"]])
+      write.table(x = peakAn, file = private$paramlist[["annoOutput"]],
+                  append = FALSE, quote = FALSE, row.names = FALSE, sep = "\t")
     }, # processing end
 
     checkRequireParam = function(){
-      if(is.null(private$paramlist[["Input"]])){
-        stop("Parameter Input is required!")
+      if(is.null(private$paramlist[["peakInput"]])){
+        stop("Parameter peakInput is required!")
       }
     },
 
     checkAllPath = function(){
-      private$checkFileExist(private$paramlist[["Input"]])
-      private$checkPathExist(private$paramlist[["Output"]])
+      private$checkFileExist(private$paramlist[["peakInput"]])
+      private$checkPathExist(private$paramlist[["annoOutput"]])
     } # checkAllPath end
 
   ) # private end
@@ -54,9 +92,21 @@ RPeakAnno <- R6::R6Class(
 #' Just for test, other parameters will be add.
 #' @param Input peak file.
 #' @param Output Output file, default Input_path/output.
-PeakAnno <- function(atacProc = NULL, Input = NULL,
-                     Output = NULL){
-  tmp <- RPeakAnno$new(atacProc, Input, Output)
+PeakAnno <- function(atacProc = NULL, peakInput = NULL, tssRegion = c(-3000, 3000), TxDb = NULL,
+                     level = "transcript", assignGenomicAnnotation = TRUE,
+                     genomicAnnotationPriority = c("Promoter", "5UTR", "3UTR", "Exon", "Intron",
+                                                   "Downstream", "Intergenic"),
+                     annoDb = NULL, addFlankGeneInfo = FALSE,
+                     flankDistance = 5000, sameStrand = FALSE, ignoreOverlap = FALSE,
+                     ignoreUpstream = FALSE, ignoreDownstream = FALSE, overlap = "TSS",
+                     verbose = TRUE, annoOutput = NULL){
+  tmp <- RPeakAnno$new(atacProc, peakInput, tssRegion, TxDb,
+                       level, assignGenomicAnnotation,
+                       genomicAnnotationPriority,
+                       annoDb, addFlankGeneInfo,
+                       flankDistance, sameStrand, ignoreOverlap,
+                       ignoreUpstream, ignoreDownstream, overlap,
+                       verbose, annoOutput)
   tmp$process()
   return(tmp)
 }
