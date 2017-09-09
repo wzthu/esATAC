e637cc41a4af79c08fd345807dc4d94597dd403b
wzthu <wzweizheng@qq.com>
2017-09-09 20:51:43 +0800

    merge blacklistQC and DHSQC to peakQC
    merge graph
    implement ConfigVal for download files

--- R/PeakQC.R
+++ R/PeakQC.R
@@ -1,19 +1,22 @@
-DHSQC <-R6Class(
-    classname = "DHSQC",
+PeakQC <-R6Class(
+    classname = "PeakQC",
     inherit = BaseProc,
     public = list(
-        initialize = function(atacProc, reportPrefix=NULL,bedDHS = NULL,bedInput = NULL,editable=FALSE){
-            super$initialize("DHSQC",editable,list(arg1=atacProc))
+        initialize = function(atacProc, reportPrefix=NULL,qcbedInput = c("DHS","blacklist","path/to/bed"),bedInput = NULL,editable=FALSE){
+            super$initialize("PeakQC",editable,list(arg1=atacProc))
             if(!is.null(atacProc)){
                 private$paramlist[["bedInput"]] <- atacProc$getParam("bedOutput");
                 regexProcName<-sprintf("(BED|bed|Bed|%s)",atacProc$getProcName())
             }else{
                 regexProcName<-"(BED|bed|Bed)"
             }
-            if(!is.null(bedDHS)){
-                private$paramlist[["bedDHS"]] <- bedDHS;
+            qcbedInput <- qcbedInput[1]
+            if(qcbedInput == "DHS"){
+                private$paramlist[["qcbedInput"]]<-.obtainConfigure("DHS");
+            }else if(qcbedInput == "blacklist"){
+                private$paramlist[["qcbedInput"]]<-.obtainConfigure("blacklist");
             }else{
-                private$paramlist[["bedDHS"]]<-.obtainConfigure("DHS");
+                private$paramlist[["qcbedInput"]]<-qcbedInput;
             }
 
             if(!is.null(bedInput)){
@@ -25,13 +28,12 @@ DHSQC <-R6Class(
                     prefix<-private$getBasenamePrefix(private$paramlist[["bedInput"]],regexProcName)
                     private$paramlist[["reportPrefix"]] <- file.path(.obtainConfigure("tmpdir"),paste0(prefix,".",self$getProcName(),".report"))
                 }
-                #private$paramlist[["reportPrefix"]] <- paste0(private$paramlist[["bedInput"]],".DHSQCreport");
+                #private$paramlist[["reportPrefix"]] <- paste0(private$paramlist[["bedInput"]],".BlacklistQCreport");
             }else{
                 private$paramlist[["reportPrefix"]] <- reportPrefix;
             }
             private$paramValidation()
         }
-
     ),
     private = list(
         processing = function(){
@@ -40,14 +42,15 @@ DHSQC <-R6Class(
             inputbed <- import(private$paramlist[["bedInput"]], genome = genome)
 
 
-            dhsbed<-import(private$paramlist[["bedDHS"]], genome = genome)
+            qcbedInput<-import(private$paramlist[["qcbedInput"]], genome = genome)
+
 
 
             qcval=list();
 
             qcval[["totalInput"]]<-length(inputbed)
-            qcval[["dhsInput"]]<-length(subsetByOverlaps(inputbed, dhsbed,ignore.strand = TRUE))
-            qcval[["dhsRate"]]<-qcval[["dhsInput"]]/qcval[["totalInput"]]
+            qcval[["qcbedInput"]]<-length(subsetByOverlaps(inputbed, qcbedInput,ignore.strand = TRUE))
+            qcval[["qcbedRate"]]<-qcval[["qcbedInput"]]/qcval[["totalInput"]]
 
             qcval<-as.matrix(qcval)
             write.table(qcval,file = paste0(private$paramlist[["reportPrefix"]],".txt"),sep="\t",quote = FALSE,col.names = FALSE)
@@ -57,14 +60,14 @@ DHSQC <-R6Class(
             if(is.null(private$paramlist[["bedInput"]])){
                 stop("bedInput is required.")
             }
-            if(is.null(private$paramlist[["bedDHS"]])){
-                stop("bedDHS is required.")
+            if(is.null(private$paramlist[["qcbedInput"]])){
+                stop("qcbedInput is required.")
             }
 
         },
         checkAllPath = function(){
             private$checkFileExist(private$paramlist[["bedInput"]]);
-            private$checkFileExist(private$paramlist[["bedDHS"]]);
+            private$checkFileExist(private$paramlist[["qcbedInput"]]);
             private$checkPathExist(private$paramlist[["reportPrefix"]]);
         }
     )
@@ -72,8 +75,9 @@ DHSQC <-R6Class(
 
 )
 
-atacDHSQC<-function(atacProc, reportPrefix=NULL,bedDHS = NULL,bedInput = NULL){
-    atacproc<-DHSQC$new(atacProc, reportPrefix=reportPrefix,bedDHS = bedDHS,bedInput = bedInput)
+
+atacPeakQC<-function(atacProc, reportPrefix=NULL,qcbedInput = c("DHS","blacklist","path/to/bed"), bedInput = NULL){
+    atacproc<-PeakQC$new(atacProc, reportPrefix=reportPrefix,qcbedInput = qcbedInput,bedInput = bedInput,editable=FALSE)
     atacproc$process()
     return(atacproc)
 }
