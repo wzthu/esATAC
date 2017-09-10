2d81ee07266eca1138f1ca0b156387d8597862c7
wzthu <wzweizheng@qq.com>
2017-09-10 10:36:19 +0800

    update method of FregLenDistr

--- R/FregLenDistribute.R
+++ R/FregLenDistribute.R
@@ -19,11 +19,17 @@ FregLenDistr <-R6Class(
       if(is.null(reportPrefix)){
           if(!is.null(private$paramlist[["bedInput"]])){
               prefix<-private$getBasenamePrefix(private$paramlist[["bedInput"]],regexProcName)
-              private$paramlist[["reportPrefix"]] <- file.path(.obtainConfigure("tmpdir"),paste0(prefix,".",self$getProcName(),".report"))
+              private$paramlist[["lendistrpdfOutput"]] <- file.path(.obtainConfigure("tmpdir"),paste0(prefix,".",self$getProcName(),".lendistr.pdf"))
+              private$paramlist[["lendistrtxtOutput"]] <- file.path(.obtainConfigure("tmpdir"),paste0(prefix,".",self$getProcName(),".lendistr.txt"))
+              private$paramlist[["dnagroovepdfOutput"]] <- file.path(.obtainConfigure("tmpdir"),paste0(prefix,".",self$getProcName(),".dnagroove.pdf"))
+              private$paramlist[["histonepdfOutput"]] <- file.path(.obtainConfigure("tmpdir"),paste0(prefix,".",self$getProcName(),".histone.pdf"))
           }
           #private$paramlist[["reportPrefix"]] <- paste(private$paramlist[["bedInput"]],".lenreport",sep="");
       }else{
-        private$paramlist[["reportPrefix"]] <- reportPrefix;
+        private$paramlist[["lendistrpdfOutput"]] <- paste0(reportPrefix,".lendistr.pdf")
+        private$paramlist[["lendistrtxtOutput"]] <- paste0(reportPrefix,".lendistr.txt")
+        private$paramlist[["dnagroovepdfOutput"]] <- paste0(reportPrefix,".dnagroove.pdf")
+        private$paramlist[["histonepdfOutput"]] <- paste0(reportPrefix,".histone.pdf")
       }
 
         private$paramValidation()
@@ -35,20 +41,20 @@ FregLenDistr <-R6Class(
           readslist<-read.table(file = private$paramlist[["bedInput"]],nrows = 1)
           bedcol=length(colnames(readslist))
           if(bedcol>3){
-              readslist<-read.table(file = private$paramlist[["bedInput"]],colClasses=c("character","integer","integer",rep("NULL",bedcol-3)))
+              readslist<-read.table(file = private$paramlist[["bedInput"]],colClasses = c("NULL","integer","integer",rep("NULL",bedcol-3)))
           }else{
-              readslist<-read.table(file = private$paramlist[["bedInput"]])
+              readslist<-read.table(file = private$paramlist[["bedInput"]],colClasses = c("NULL","integer","integer") )
           }
 
 
 
-          readlens<-readslist$V3-readslist$V2
+          readlens<-readslist[[2]]-readslist[[1]]
 
           #read length distribution
-          allreadslen <-as.data.frame(readlens)
-          colnames(allreadslen)<-"length"
-          ggplot(allreadslen)+geom_histogram(bins = max(allreadslen),aes(x=length))
-          ggsave(paste0(private$paramlist[["reportPrefix"]],".lendistr.pdf"))
+#          allreadslen <-as.data.frame(readlens)
+#          colnames(allreadslen)<-"length"
+#          ggplot(allreadslen)+geom_histogram(bins = max(allreadslen),aes(x=length))
+#          ggsave(paste0(private$paramlist[["reportPrefix"]],".lendistr.pdf"))
 
           #readslen<-names(readscounts)
           #readslen<-cbind(readlen,readscounts)
@@ -61,12 +67,16 @@ FregLenDistr <-R6Class(
           readscounts<-data.frame(readscounts)
           colnames(readscounts)<-c("length","counts")
 
-          write.table(x=readscounts,file = paste0(private$paramlist[["reportPrefix"]],".lendistr.txt"),quote = FALSE,row.names = FALSE,sep="\t")
+          write.table(x=readscounts,file = private$paramlist[["lendistrtxtOutput"]],quote = FALSE,row.names = FALSE,sep="\t")
 
           readscounts$length=as.integer(as.character(readscounts$length))
           mg<-data.frame(length=1:max(readscounts$length))
           readscounts <- merge(readscounts,mg,by="length",all = TRUE)
           readscounts$counts[is.na(readscounts$counts)]<-0
+          
+          ggplot(readscounts, aes(length))+geom_ribbon(aes(ymin=0, ymax=counts))
+          ggsave(paste0(private$paramlist[["reportPrefix"]],".lendistr.pdf"))
+          
           rs<-Mod(fft(readscounts$counts))/length(readscounts$counts)
           t<-length(readscounts$counts)/(1:(length(readscounts$counts)-1))
           rs<-rs[2:length(rs)]
@@ -96,7 +106,7 @@ FregLenDistr <-R6Class(
               #ggplot(rs1,aes(x=perior,y=strength))+geom_area(aes(fill="valence",color=check))
               checkdna=1
               ggplot(rs1)+geom_ribbon(data=subset(rs1,perior<=min(rs1$perior[rs1$check==checkdna])),aes(x=perior,ymin=0,ymax=strength),fill="blue")+geom_ribbon(data=subset(rs1,perior>=max(rs1$perior[rs1$check==checkdna])),aes(x=perior,ymin=0,ymax=strength),fill="blue")+geom_ribbon(data=subset(rs1,check==checkdna),aes(x=perior,ymin=0,ymax=strength),fill="red")
-              ggsave(paste0(private$paramlist[["reportPrefix"]],".dnagroove.pdf"))
+              ggsave(private$paramlist[["dnagroovepdfOutput"]])
           }
           if(length(t)>220){
               rs2<-as.data.frame(cbind(t[t<400&t>2],rs[t<400&t>2],tp[t<400&t>2]))
@@ -107,7 +117,7 @@ FregLenDistr <-R6Class(
               #ggplot(allreadslen)+geom_density(aes(x="strength",fill="clarity"))
               checkhistone=2
               ggplot(rs2)+geom_ribbon(data=subset(rs2,perior<=min(rs2$perior[rs2$check==checkhistone])),aes(x=perior,ymin=0,ymax=strength),fill="blue")+geom_ribbon(data=subset(rs2,perior>=max(rs2$perior[rs2$check==checkhistone])),aes(x=perior,ymin=0,ymax=strength),fill="blue")+geom_ribbon(data=subset(rs2,check==checkhistone),aes(x=perior,ymin=0,ymax=strength),fill="red")
-              ggsave(paste0(private$paramlist[["reportPrefix"]],".histone.pdf"))
+              ggsave(private$paramlist[["histonepdfOutput"]])
                 
           }
 
